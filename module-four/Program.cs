ae151ee427e683f4bd00194cece58e2922b0109f
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-11-29 15:53:01 +0530

    Add project files.

---
+++ module-four/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
